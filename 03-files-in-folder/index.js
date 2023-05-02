6a73ad1e173f1b9d207880104eb411cda0d80360
Elvira <sabirova.elvira.gam@gmail.com>
2023-05-02 15:02:47 +0500

    feat: solve the 3th task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const folder = path.join(__dirname, 'secret-folder')
+fs.readdir(folder, { withFileTypes: true }, (err, files) => {
+  files.forEach(file => {
+    let inf = ''
+    if (file.isFile()) {
+      fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, stats) => {
+        inf = `${file.name.split('.')[0]} - ${path.extname(file.name).split('.')[1]} - ${stats.size.toString()}b`
+        console.log(inf);
+      })
+
+    }
+  });
+});
+
