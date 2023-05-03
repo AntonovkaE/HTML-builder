7d3f33f248e9e0f0b85ff347a950410c268664ab
Elvira <sabirova.elvira.gam@gmail.com>
2023-05-03 13:40:41 +0500

    feat: solve the 5-th task merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const folder = path.join(__dirname, 'styles');
+fs.readdir(folder, { withFileTypes: true }, (err, files) => {
+  let inf = [];
+  files.forEach(file => {
+    if (file.isFile() && path.extname(file.name) === '.css') {
+      inf.push(fs.readFileSync(path.join(__dirname, 'styles', file.name), 'utf8'));
+    }
+  });
+  fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'),
+    inf.join('\n'),
+    function (error) {
+      if (error) throw error; // если возникла ошибка
+      console.log('Запись файла завершена');
+    });
+});
